0f82d46ebc95f6aa142b62a3341dbdc63a9d77f1
patrikeevairina <59138641+patrikeevairina@users.noreply.github.com>
2020-04-26 14:21:58 +0300

    Create StackImplementation.h

---
+++ StackImplementation.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+
+using ValueType = double;
+
+// интерфейс для конкретных реализаций контейнера для стека
+class StackImplementation {
+public:
+    // добавление в хвост
+    virtual void push(const ValueType& value) = 0;
+    // удаление с хвоста
+    virtual void pop() = 0;
+    // посмотреть элемент в хвосте
+    virtual ValueType& top() = 0;
+    virtual const ValueType& top() const = 0;
+    // проверка на пустоту
+    virtual bool isEmpty() const = 0;
+    // размер
+    virtual size_t size() const = 0;
+    // виртуальный деструктор
+    virtual ~StackImplementation() { /*std::cout << " remove stack_imp" << std::endl; */ };
+};
