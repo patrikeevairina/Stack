a5943fd9b977fdd442bcf7af9e60acf1f207736c
patrikeevairina <59138641+patrikeevairina@users.noreply.github.com>
2020-05-21 17:01:36 +0300

    Update stack.h

--- stack.h
+++ stack.h
@@ -1,10 +1,14 @@
 #include <iostream>
+#include "liststack.h"
+#include "vectorstack.h"
+#include "StackImplementation.h"
+
 #pragma once
 // Уровень абстракции
 // клиентский код подключает именно этот хедер
 
 // тип значений в стеке
-using ValueType = double;
+
 
 // на основе какого контейнера работает стек
 enum class StackContainer {
@@ -14,16 +18,17 @@ enum class StackContainer {
 };
 
 // декларация класса с реализацией
-class StackImplementation;
+//class StackImplementation;
 
+template <typename ValueType>
 class Stack
 {
 public:
     // Большая пятерка
-    Stack(StackContainer container = StackContainer::Vector);
+    Stack(StackContainer container = StackContainer::List);
     // элементы массива последовательно подкладываются в стек
     Stack(const ValueType* valueArray, const size_t arraySize,
-          StackContainer container = StackContainer::Vector);
+          StackContainer container = StackContainer::List);
 
     explicit Stack(const Stack& copyStack);
     Stack& operator=(const Stack& copyStack);
@@ -47,7 +52,162 @@ public:
     size_t size() const;
 private:
     // указатель на имплементацию (уровень реализации)
-    StackImplementation* _pimpl = nullptr;
+    StackImplementation<ValueType>* _pimpl = nullptr;
     // тип контейнера, наверняка понадобится
     StackContainer _containerType;
 };
+
+template <typename ValueType>
+Stack<ValueType>::Stack(StackContainer container)
+    : _containerType(container)
+{
+    switch (container)
+    {
+    case StackContainer::List: {
+        _pimpl = new ListStack<ValueType>();
+        break;
+    }
+    case StackContainer::Vector: {
+        _pimpl = new VectorStack<ValueType>();
+        break;
+    }
+    default:
+        throw std::runtime_error("Неизвестный тип контейнера");
+    }
+  //  std::cout << this->size() << " size " << std::endl;
+}
+
+template <typename ValueType>
+Stack<ValueType>::Stack(const ValueType* valueArray, const size_t arraySize, StackContainer container)
+    : _containerType(container)
+{
+    switch (container)
+    {
+    case StackContainer::List: {
+        _pimpl = new ListStack<ValueType>();
+        break;
+    }
+    case StackContainer::Vector: {
+        _pimpl = new VectorStack<ValueType>();
+        break;
+    }
+    default:
+        throw std::runtime_error("Неизвестный тип контейнера");
+    }
+    for (size_t i = 0; i < arraySize; i++)
+    {
+        this->push(valueArray[i]);
+    }
+}
+
+template <typename ValueType>
+Stack<ValueType>::Stack(const Stack& copyStack)
+{
+    _containerType = copyStack._containerType;
+    switch (_containerType)
+    {
+    case StackContainer::List: {
+        _pimpl = new ListStack<ValueType>(*(static_cast<ListStack<ValueType>*>(copyStack._pimpl)));	// конкретизируйте под ваши конструкторы, если надо
+        break;
+    }
+    case StackContainer::Vector: {
+        _pimpl = new VectorStack<ValueType>(*(static_cast<VectorStack<ValueType>*>(copyStack._pimpl)));	// конкретизируйте под ваши конструкторы, если надо
+        break;
+    }
+    default:
+        throw std::runtime_error("Неизвестный тип контейнера");
+    }
+}
+
+template <typename ValueType>
+Stack<ValueType>& Stack<ValueType>::operator=(const Stack& copyStack)
+{
+    if (this == &copyStack)
+        return *this;
+    _containerType = copyStack._containerType;
+    delete _pimpl;
+    switch (_containerType)
+    {
+    case StackContainer::List: {
+        _pimpl = new ListStack<ValueType>(*(static_cast<ListStack<ValueType>*>(copyStack._pimpl)));	// конкретизируйте под ваши конструкторы, если надо
+        break;
+    }
+    case StackContainer::Vector: {
+        _pimpl = new VectorStack<ValueType>(*(static_cast<VectorStack<ValueType>*>(copyStack._pimpl)));	// конкретизируйте под ваши конструкторы, если надо
+        break;
+    }
+    default:
+        throw std::runtime_error("Неизвестный тип контейнера");
+    }
+    return *this;
+}
+
+template <typename ValueType>
+Stack<ValueType>::Stack(Stack&& moveStack) noexcept
+{
+    _containerType = moveStack._containerType;
+    delete _pimpl;
+    _pimpl = moveStack._pimpl;
+
+    moveStack._pimpl = nullptr;
+}
+
+template <typename ValueType>
+Stack<ValueType>& Stack<ValueType>::operator=(Stack &&moveStack) noexcept
+{
+    if (this == &moveStack)
+    {
+        return *this;
+    }
+    delete _pimpl;
+    _containerType = moveStack._containerType;
+    _pimpl = moveStack._pimpl;
+
+    moveStack._pimpl = nullptr;
+
+    return *this;
+}
+
+template <typename ValueType>
+Stack<ValueType>::~Stack()
+{
+    delete _pimpl;		// композиция!
+}
+
+template <typename ValueType>
+void Stack<ValueType>::push(const ValueType& value)
+{
+    _pimpl->push(value);
+}
+
+template <typename ValueType>
+void Stack<ValueType>::pop()
+{
+    _pimpl->pop();
+}
+
+template <typename ValueType>
+ValueType& Stack<ValueType>::top()
+{
+    return _pimpl->top();
+}
+
+template <typename ValueType>
+const ValueType& Stack<ValueType>::top() const
+{
+    return _pimpl->top();
+}
+
+template <typename ValueType>
+bool Stack<ValueType>::isEmpty() const
+{
+    return _pimpl->isEmpty();
+}
+
+template <typename ValueType>
+size_t Stack<ValueType>::size() const
+{
+    return _pimpl->size();
+}
+
+
