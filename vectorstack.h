cf67ed385a27dc28f4b43d46b7fa2facb04d67ea
patrikeevairina <59138641+patrikeevairina@users.noreply.github.com>
2020-05-21 17:00:30 +0300

    Update vectorstack.h

--- vectorstack.h
+++ vectorstack.h
@@ -1,11 +1,12 @@
 #pragma once
 #include "StackImplementation.h"
-#include "C:/Programming/vector/vector.h" 
+#include "vector.h"
 
-class VectorStack : public StackImplementation, public MyVector
+template <typename ValueType>
+class VectorStack : public StackImplementation<ValueType>, public MyVector<ValueType>
 {
 public:
-    VectorStack() { std::cout << "creating vector" << std::endl; }
+    VectorStack() {}
     VectorStack(const VectorStack& copyVector);
     void push(const ValueType &value) override;
     void pop() override;
@@ -14,5 +15,48 @@ public:
     bool isEmpty() const override;
     size_t size() const override;
 
-    ~VectorStack(){ /*std::cout << "remove vector" << std::endl;*/ };
+    ~VectorStack(){};
 };
+
+template <typename ValueType>
+VectorStack<ValueType>::VectorStack(const VectorStack &copy) : MyVector<ValueType>(copy)
+{}
+
+template <typename ValueType>
+void VectorStack<ValueType>::push(const ValueType &value)
+{   
+    this->pushBack(value);
+}
+
+template <typename ValueType>
+void VectorStack<ValueType>::pop()
+{
+    this->popBack();
+}
+
+template <typename ValueType>
+ValueType& VectorStack<ValueType>::top()
+{
+    return (*this)[size()-1];
+}
+
+template <typename ValueType>
+const ValueType& VectorStack<ValueType>::top() const
+{
+    return (*this)[size()-1];
+}
+
+template <typename ValueType>
+bool VectorStack<ValueType>::isEmpty() const
+{
+    if (size() == 0)
+        return true;
+    else
+        return false;
+}
+
+template <typename ValueType>
+size_t VectorStack<ValueType>::size() const
+{
+    return this->MyVector<ValueType>::size();
+}
